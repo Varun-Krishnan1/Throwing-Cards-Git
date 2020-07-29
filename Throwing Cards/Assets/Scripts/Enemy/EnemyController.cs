d47202b941f2f2395330619cbdcd97c9f5df8962
SpadeXavier <2017.vkrishnan@gmail.com>
2020-07-29 13:05:37 -0400

    Added patrol ability to enemy as well as fixed animation so it always does it at 180
    This will need to be fixed later so that the animation's rotation for the death effect is the same as the dying enemy. This is because the parent object is 0,0 while it's children are at different positions so doing a rotation massively affects the children's position

--- Throwing Cards/Assets/Scripts/Enemy/EnemyController.cs
+++ Throwing Cards/Assets/Scripts/Enemy/EnemyController.cs
@@ -1,16 +1,26 @@
 ﻿using System; 
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Security.Cryptography;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public int health;  
-    public float moveSpeedWhenHit;
     public float fHorizontalDampingWhenMoving;
     public float knockbackForce;
     public HealthBar healthBar;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    [Range(0, 1)] [SerializeField] private float fHorizontalDampingBasic = .22f;     // How much to smooth out the movement
+    public Transform groundDetection;
+    public float checkDistance;
+    public bool movingRight = false;
+
+    private Rigidbody2D rb;
+
 
     [Header("Death Effect")]
     public GameObject deathEffect;
@@ -20,9 +30,7 @@ public class EnemyController : MonoBehaviour
 
     private GameObject player;
 
-    private float moveSpeed;
-    private bool moveRight = false; 
-    private Rigidbody2D rb;
+
     private Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -30,7 +38,6 @@ public class EnemyController : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
         //animator = this.GetComponent<Animator>();
         healthBar.SetMaxHealth(health);
-        player = GameObject.FindWithTag("Player");
 
     }
 
@@ -38,9 +45,37 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, checkDistance);
+        RaycastHit2D wallInfo = Physics2D.Raycast(groundDetection.position, Vector2.right, checkDistance);
+        print(groundInfo.transform); 
+        if(groundInfo.collider == false)
+        {
+            print("Edge of surface");  
+            transform.Rotate(0, 180f, 0);
+            movingRight = !movingRight;
+        }
 
-
+        Move();
     }
+
+    void Move()
+    {
+        float fHorizontalVelocity = rb.velocity.x;
+
+        
+        if (movingRight)
+        {
+            fHorizontalVelocity += .5f;
+        }
+        else
+        {
+            fHorizontalVelocity -= .5f;
+        }
+
+        fHorizontalVelocity *= (float)Math.Pow(1f - fHorizontalDampingBasic, Time.deltaTime * 10f);
+        rb.velocity = new Vector2(fHorizontalVelocity, rb.velocity.y);
+    }
+    
 
 
     public void TakeDamage(int damage, GameObject weapon)
@@ -70,7 +105,7 @@ public class EnemyController : MonoBehaviour
     public void Die()
     {
         print(this.gameObject.transform.position);
-        GameObject e = Instantiate(deathEffect, new Vector3(transform.position.x + offsetX, transform.position.y + offsetY, 0), this.gameObject.transform.rotation);
+        GameObject e = Instantiate(deathEffect, new Vector3(transform.position.x + offsetX, transform.position.y + offsetY, 0), Quaternion.Euler(0,180,0));
         print(e.transform.position);
         Destroy(gameObject);
     }
