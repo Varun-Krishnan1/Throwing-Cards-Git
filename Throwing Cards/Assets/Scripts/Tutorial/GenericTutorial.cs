1bba5628f55c14baf749b0367f08a29c7afc107f
SpadeXavier <2017.vkrishnan@gmail.com>
2020-08-03 12:00:22 -0400

    Added roll and wall climbing tutorial

---
+++ Throwing Cards/Assets/Scripts/Tutorial/GenericTutorial.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTutorial : TutorialCanvasController
+{
+    protected override void OnActionDestroy()
+    {
+
+    }
+}
---
+++ Throwing Cards/Assets/Scripts/Tutorial/RollTutorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollTutorial : TutorialCanvasController
+{
+    public Animator playerAnimator; 
+    protected override void OnActionDestroy()
+    {
+        if (playerAnimator.GetBool("isRolling")) {
+            Destroy(this.gameObject); 
+        }
+    }
+}
